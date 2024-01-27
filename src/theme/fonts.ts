c7f259e4dfbaa324c67ce10f375507922d928c6e
Mai Xuân Phi <101462441+MaiXuanPhi001@users.noreply.github.com>
2024-01-27 11:39:53 +0700

    Add BinancePlex-Light font

--- src/theme/fonts.ts
+++ src/theme/fonts.ts
@@ -18,6 +18,7 @@ export const fonts = {
     SAN: 'OpenSans-Light',
     SANM: 'OpenSans-Medium',
     SANB: 'OpenSans-Bold',
+    BNPL: 'BinancePlex-Light',
     BNPM: 'BinancePlex-Medium',
     BNPR: 'BinancePlex-Regular',
     BNPSB: 'BinancePlex-SemiBold',
